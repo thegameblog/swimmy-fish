f908066fd35783013c155673831d9dcde3e1044a
Joe Esposito <joe@joeyespo.com>
2015-05-06 22:52:44 -0400

    Spacing in helpers.

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 module.exports = {
-  randInt: function(min, max) {
+  randInt: function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   },
   fillCircle: function (ctx, x, y, r, color) {
@@ -27,7 +27,7 @@ module.exports = {
     ctx.fillStyle = outline;
     ctx.fillText(text, x, y);
   },
-  intersected: function(rect1, rect2) {
+  intersected: function (rect1, rect2) {
     return (rect1.x < rect2.x + rect2.width &&
       rect1.x + rect1.width > rect2.x &&
       rect1.y < rect2.y + rect2.height &&
